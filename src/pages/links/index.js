b1e67df4434d31df1616773fe6ef38cbf4ae6aad
Kuan Zhou <kzhoulatte@LATTE-Lab.local>
2023-08-18 14:59:43 -0700

    update links

--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -7,21 +7,24 @@ const BlogPage = ({ data }) => {
     <Layout pageTitle="Helpful links">
       <ul>
       <li>
+      <h3> Daily news </h3>
+      <ul>
+        <li><a href="https://news.ycombinator.com/">Hacker News</a></li>
+        <li><a href="https://substack.com/notes">Substack</a></li>
+        <li><a href="https://www.bloomberg.com/">Bloomberg</a></li>
+        <li><a href="https://www.teamblind.com/">TeamBlind</a></li>
+        <li><a href="https://finance.yahoo.com/">Yahoo Finance</a></li>
+      
+      </ul>
+      
+      </li>
+      <li>
       <h3> Social media </h3>
       <ul>
         <li> <a href="https://twitter.com/">Twitter</a></li>
         <li> <a href="https://www.linkedin.com/feed/">Linkedin</a> </li>
         <li><a href="https://www.zhihu.com/follow">Zhihu</a></li>
         
-      </ul>
-      </li>
-      <li>
-      <h3> Daily news </h3>
-      <ul>
-        <li><a href="https://www.bloomberg.com/">Bloomberg</a></li>
-        <li><a href="https://www.teamblind.com/">TeamBlind</a></li>
-        <li><a href="https://finance.yahoo.com/">Yahoo Finance</a></li>
-      
       </ul>
       </li>
       
